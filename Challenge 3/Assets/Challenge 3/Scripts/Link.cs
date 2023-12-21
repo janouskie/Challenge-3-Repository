73c17b8fdde4cdd1f6ac8c48a148d234a5a3f501
janouskie <b00149565@mytudublin.ie>
2023-12-21 19:29:18 +0000

    yay!

---
+++ Challenge 3/Assets/Challenge 3/Scripts/Link.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenGithub()
+    {
+        Application.OpenURL("https://github.com/janouskie/SubmissionChallenge3");
+    }
+}
